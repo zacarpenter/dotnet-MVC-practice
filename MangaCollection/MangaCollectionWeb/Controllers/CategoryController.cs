60ea5fa15e36dddbafcbf06c9e1d03e7f11aef42
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2022-07-28 16:02:09 -0400

    Added MangaCollection project

---
+++ MangaCollection/MangaCollectionWeb/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using MangaCollectionWeb.Data;
+using MangaCollectionWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MangaCollectionWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategorList = _db.Categories;
+            return View(objCategorList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+          return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category cat)
+        {
+            _db.Categories.Add(cat);
+            _db.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ MangaCollection/MangaCollectionWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using MangaCollectionWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MangaCollectionWeb.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
