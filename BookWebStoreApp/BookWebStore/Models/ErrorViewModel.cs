486d5d1b4c7bcf94110faeb94eed00c464a845ca
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2022-08-12 13:24:31 -0400

    Initial commit - BookStoreWebApp

---
+++ BookWebStoreApp/BookWebStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookWebStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
