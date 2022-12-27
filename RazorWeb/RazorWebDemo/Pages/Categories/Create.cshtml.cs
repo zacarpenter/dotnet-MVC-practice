a04769cf991db3cc64c892f2624c050398aa0bc4
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2022-12-27 12:01:28 -0500

    added bootstrap theme, added form validation

--- RazorWeb/RazorWebDemo/Pages/Categories/Create.cshtml.cs
+++ RazorWeb/RazorWebDemo/Pages/Categories/Create.cshtml.cs
@@ -1,14 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorWebDemo.Data;
 using RazorWebDemo.Model;
 
 namespace RazorWebDemo.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDBContext _db;
         public Category Category { get; set; }
+        public CreateModel(ApplicationDBContext db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Category.AddAsync(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
         }
     }
 }
